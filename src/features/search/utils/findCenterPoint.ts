a99fb6af51521373861409e0431b4c100b081833
Khang Vo <kvo189@gmail.com>
2023-10-04 21:48:11 -0500

    Remove unused util functions

--- src/features/search/utils/findCenterPoint.ts
+++
@@ -1,8 +0,0 @@
-import { Location } from '..';
-
-export const findCenterPoint = (locations: Location[]): { lat: number; lon: number } => {
-    const totalLocations = locations.length;
-    const latitude = locations.reduce((sum, loc) => sum + loc.latitude, 0) / totalLocations;
-    const longitude = locations.reduce((sum, loc) => sum + loc.longitude, 0) / totalLocations;
-    return { lat: latitude, lon: longitude };
-};
--- src/features/search/utils/isStateAbbreviation.ts
+++
@@ -1,5 +0,0 @@
-import { STATES, StateAbbreviation } from '../types/StateAbbreviation';
-// Type guard to check if a string is a StateAbbreviation
-export function isStateAbbreviation(str: string): str is StateAbbreviation {
-  return STATES.includes(str as StateAbbreviation);
-}
